d4670745458e9008a81e34e8436128a95885feef
jhamPac <cloud@kevia.me>
2021-09-29 10:43:37 -0700

    feat: basic db saving in haskell, yay!

--- src/Query.hs
+++ src/Query.hs
@@ -3,4 +3,11 @@ module Query where
 import           Database.HDBC
 import           Database.HDBC.Sqlite3 (connectSqlite3)
 
-greet = 8 + 8
+query :: IO ()
+query =
+    do
+        conn <- connectSqlite3 "h.db"
+        run conn "CREATE TABLE test (id INTEGER NOT NULL, desc VARCHAR(80))" []
+        run conn "INSERT INTO test VALUES (?, ?)" [toSql "0", toSql "zero"]
+        commit conn
+        disconnect conn
