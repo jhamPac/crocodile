397b89b2b2cdb1a8456ef81d3c0a4280988ad1cf
jhamPac <cloud@kevia.me>
2021-10-18 09:24:30 -0700

    chore: install xml parsing dep

--- src/PodParser.hs
+++ src/PodParser.hs
@@ -1,3 +1,10 @@
 module PodParser where
 
+import           Data.Char
+import           Data.List
 import           PodTypes
+import           Text.XML.HaXml
+import           Text.XML.HaXml.Html.Generate (showattr)
+import           Text.XML.HaXml.Parse
+
+data PodItem = PodItem {itemtitle :: String, enclosureURL :: String} deriving (Eq, Show, Read)
