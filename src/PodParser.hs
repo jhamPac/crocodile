68e08b64500a2429834ca004a40b9b3d4b2df43b
jhamPac <cloud@kevia.me>
2021-10-18 10:01:06 -0700

    chore: it works!

--- src/PodParser.hs
+++ src/PodParser.hs
@@ -6,6 +6,7 @@ import           PodTypes
 import           Text.XML.HaXml
 import           Text.XML.HaXml.Html.Generate (showattr)
 import           Text.XML.HaXml.Parse
+import           Text.XML.HaXml.Posn
 
 data PodItem = PodItem {
                         itemTitle      :: String
@@ -28,11 +29,11 @@ parse :: String -> String -> Feed
 parse content name =
     Feed {channelTitle = getTitle doc, items = getEnclosures doc}
     where
-        parseResult = xmlParse name (stripUnicodeBOM content)
+        parseResult = xmlParse name $ stripUnicodeBOM content
         doc = getContent parseResult
 
-        getContent :: Document -> Content
-        getContent (Document _ _ e _) = CElem e
+        getContent :: Document Posn -> Content Posn
+        getContent (Document _ _ e _) = CElem e noPos
 
         {- | Some Unicode documents begin with a binary sequence;
              strip it off before processing. -}
@@ -40,31 +41,46 @@ parse content name =
         stripUnicodeBOM ('\xef':'\xbb':'\xbf':x) = x
         stripUnicodeBOM x                        = x
 
-channel :: CFilter
+channel :: CFilter Posn
 channel = tag "rss" /> tag "channel"
 
-getTitle :: Content -> String
+getTitle :: Content Posn -> String
 getTitle doc =
     contentToStringDefault "Untitled Podcast" (channel /> tag "title" /> txt $ doc)
 
-getEnclosures :: Content -> [PodItem]
+getEnclosures :: Content Posn -> [PodItem]
 getEnclosures doc =
     concatMap procPodItem $ getPodItems doc
     where
-        procPodItem :: Content -> [PodItem]
+        procPodItem :: Content Posn -> [PodItem]
         procPodItem item = concatMap (procEnclosure title) enclosure
             where
                 title = contentToStringDefault "Untitled Episode" (keep /> tag "title" /> txt $ item)
                 enclosure = (keep /> tag "enclosure") item
 
-        getPodItems :: CFilter
+        getPodItems :: CFilter Posn
         getPodItems = channel /> tag "item"
 
-        procEnclosure :: String -> Content -> [PodItem]
+        procEnclosure :: String -> Content Posn -> [PodItem]
         procEnclosure title enclosure =
             map makePodItem (showattr "url" enclosure)
             where
-                makePodItem :: Content -> PodItem
+                makePodItem :: Content Posn -> PodItem
                 makePodItem x = PodItem {itemTitle = title, enclosureURL = contentToString [x]}
 
+contentToStringDefault :: String -> [Content Posn] -> String
+contentToStringDefault msg [] = msg
+contentToStringDefault _ x    = contentToString x
 
+contentToString :: [Content Posn] -> String
+contentToString =
+    concatMap procContent
+    where
+        procContent x =
+            verbatim $ keep /> txt $ CElem (unesc . fakeElem $ x) noPos
+
+        fakeElem :: Content Posn -> Element Posn
+        fakeElem x = Elem (N "fake") [] [x]
+
+        unesc :: Element Posn -> Element Posn
+        unesc = xmlUnEscape stdXmlEscaper
