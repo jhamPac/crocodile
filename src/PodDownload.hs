4315ba088c9211e36aed0b853a13013b0a2bc2cf
jhamPac <cloud@kevia.me>
2021-10-20 10:27:38 -0700

    feat: download module complete

--- src/PodDownload.hs
+++ src/PodDownload.hs
@@ -10,4 +10,55 @@ import           PodTypes
 import           System.IO
 
 downloadURL :: String -> IO (Either String String)
-downloadURL = undefined
+downloadURL url = do
+    resp <- simpleHTTP request
+    case resp of
+        Left x -> pure $ Left ("Error connecting: " ++ show x)
+        Right r ->
+            case rspCode r of
+                (2, _, _) -> pure $ Right (rspBody r)
+                (3, _, _) ->
+                    case findHeader HdrLocation r of
+                        Nothing  -> pure $ Left (show r)
+                        Just url -> downloadURL url
+                _ -> pure $ Left (show r)
+    where
+        request = Request { rqURI = uri
+                          , rqMethod = GET
+                          , rqHeaders = []
+                          , rqBody = "" }
+        uri = fromJust $ parseURI url
+
+updatePodcastFromFeed :: IConnection conn => conn -> Podcast -> IO ()
+updatePodcastFromFeed conn pc = do
+    resp <- downloadURL (castURL pc)
+    case resp of
+        Left x    -> putStrLn x
+        Right doc -> updateDB doc
+
+    where
+        updateDB doc = do
+            mapM_ (addEpisode conn) episodes
+            commit conn
+
+            where
+                feed = parse doc (castURL pc)
+                episodes = map (item2ep pc) (items feed)
+
+getEpisode :: IConnection conn => conn -> Episode -> IO (Maybe String)
+getEpisode conn ep = do
+    resp <- downloadURL (epURL ep)
+    case resp of
+        Left x -> do
+                    putStrLn x
+                    pure Nothing
+        Right doc -> do
+            file <- openBinaryFile filename WriteMode
+            hPutStr file doc
+            hClose file
+            updateEpisode conn (ep {epDone = True})
+            commit conn
+            pure (Just filename)
+
+    where
+        filename = "pod." ++ (show . castID . epCast $ ep) ++ "." ++ show (epID ep) ++ ".mp3"
